418eca5f215af78807c6ed46268f2263489b11b6
mansha srivastava <59260493+123mansha@users.noreply.github.com>
2023-09-14 11:44:45 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionoperations.js
@@ -0,0 +1,40 @@
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// let newScore = "33abc"
+
+// console.log(typeof newScore)
+
+// let newValueInNumber = Number(newScore)
+// console.log(typeof newValueInNumber)
+// console.log(newValueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+// let isLoggedIn = 1
+
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn)
+
+// let isLoggedIn = ""
+
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn)
+
+//1 => true ; 0 => false
+//"" => false
+//"hitesh" => true
+
+let randNum = 33
+
+let randNumString = String(randNum)
+console.log(randNumString);
+console.log(typeof randNumString);
