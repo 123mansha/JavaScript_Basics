24e1e02816edb5d938f5683436601060ef6fa842
mansha srivastava <59260493+123mansha@users.noreply.github.com>
2023-09-14 11:00:02 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code a newer version(not need to write it but a good practice)
+
+// alert(hello); we r using node JS not browser :error
+
+let name = "mansha"
+let age = 23
+let isloggedIn = false
+
+//number => 2^53
+//bigint => 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned but variable is declared.
+//symbol => unique
+
+//objects
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
+
